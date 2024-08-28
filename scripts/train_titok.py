e4cb22dac7e8b181f1373d130e27f2532f810553
Qihang Yu <yucornetto@gmail.com>
2024-08-28 15:19:00 -0700

    Update train_titok.py

--- scripts/train_titok.py
+++ scripts/train_titok.py
@@ -16,19 +16,6 @@ limitations under the License.
 
 Reference:
     https://github.com/huggingface/open-muse
-
-Example Command:
-WANDB_MODE=offline WORKSPACE=/opt/tiger/1d_tokenization accelerate launch --num_machines=1 --num_processes=8 --machine_rank=0 --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network scripts/train_titok.py config=configs/stage1/titok_b64.yaml \
-    experiment.project="titok_b64_stage1" \
-    experiment.name="titok_b64_stage1_run1" \
-    experiment.output_dir="titok_b64_stage1_run1" \
-    training.per_gpu_batch_size=32
-
-WANDB_MODE=offline WORKSPACE=/opt/tiger/1d_tokenization accelerate launch --num_machines=1 --num_processes=8 --machine_rank=0 --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network scripts/train_titok.py config=configs/stage2/titok_b64.yaml \
-    experiment.project="titok_b64_stage2" \
-    experiment.name="titok_b64_stage2_run1" \
-    experiment.output_dir="titok_b64_stage2_run1" \
-    training.per_gpu_batch_size=32
 """
 import math
 import os
