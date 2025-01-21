cf82eb66c26cf1d8382fec59ea754ed0bb757f13
TACJu <jhe47@jh.edu>
2025-01-21 14:51:41 -0800

    update perceptual loss with LPIPS

--- modeling/modules/lpips.py
+++ modeling/modules/lpips.py
@@ -5,10 +5,15 @@ All Bytedance's Modifications are Copyright (year) Bytedance Ltd. and/or its aff
 
 Reference:
     https://github.com/richzhang/PerceptualSimilarity/
+    https://github.com/CompVis/taming-transformers/blob/master/taming/modules/losses/lpips.py
+    https://github.com/CompVis/taming-transformers/blob/master/taming/util.py
 """
 
 import os
+import hashlib
+import requests
 from collections import namedtuple
+from tqdm import tqdm
 
 import torch
 import torch.nn as nn
@@ -17,6 +22,49 @@ from torchvision import models
 
 _LPIPS_MEAN = [-0.030, -0.088, -0.188]
 _LPIPS_STD = [0.458, 0.448, 0.450]
+
+
+URL_MAP = {
+    "vgg_lpips": "https://heibox.uni-heidelberg.de/f/607503859c864bc1b30b/?dl=1"
+}
+
+CKPT_MAP = {
+    "vgg_lpips": "vgg.pth"
+}
+
+MD5_MAP = {
+    "vgg_lpips": "d507d7349b931f0638a25a48a722f98a"
+}
+
+
+def download(url, local_path, chunk_size=1024):
+    os.makedirs(os.path.split(local_path)[0], exist_ok=True)
+    with requests.get(url, stream=True) as r:
+        total_size = int(r.headers.get("content-length", 0))
+        with tqdm(total=total_size, unit="B", unit_scale=True) as pbar:
+            with open(local_path, "wb") as f:
+                for data in r.iter_content(chunk_size=chunk_size):
+                    if data:
+                        f.write(data)
+                        pbar.update(chunk_size)
+
+
+def md5_hash(path):
+    with open(path, "rb") as f:
+        content = f.read()
+    return hashlib.md5(content).hexdigest()
+
+
+def get_ckpt_path(name, root, check=False):
+    assert name in URL_MAP
+    path = os.path.join(root, CKPT_MAP[name])
+    if not os.path.exists(path) or (check and not md5_hash(path) == MD5_MAP[name]):
+        print("Downloading {} model from {} to {}".format(name, URL_MAP[name], path))
+        download(URL_MAP[name], path)
+        md5 = md5_hash(path)
+        assert md5 == MD5_MAP[name], md5
+    return path
+
 
 class LPIPS(nn.Module):
     # Learned perceptual metric.
@@ -37,7 +85,7 @@ class LPIPS(nn.Module):
     def load_pretrained(self):
         workspace = os.environ.get('WORKSPACE', '')
         # Downloaded from OpenMUSE on 09/28/23.
-        VGG_PATH = os.path.join(workspace, "models/vgg_lpips.pth")
+        VGG_PATH = get_ckpt_path("vgg_lpips", os.path.join(workspace, "models/vgg_lpips.pth"), check=True)
         self.load_state_dict(torch.load(VGG_PATH, map_location=torch.device("cpu")), strict=False)
 
     def forward(self, input, target):
